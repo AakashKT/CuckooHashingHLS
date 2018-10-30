3ea5a1801db3dfaf5c5f7035e4ff40012c932a68
Siddharth Bhat <siddu.druid@gmail.com>
2018-10-30 22:07:12 +0530

    move test files around

---
+++ hls/hashing/execute_test.cpp
@@ -0,0 +1,14 @@
+#include "jenkins_hash.h"
+#include "search.h"
+#include "execute.h"
+#include <assert.h>
+
+int main()
+{
+	// stored in BRAM
+	KMetadata key_to_metadata[NUM_HASH_TABLES][HASH_TABLE_SIZE],
+	// stored in DRAM: (key, value)
+	KV key_to_val[NUM_HASH_TABLES][HASH_TABLE_SIZE]);
+
+
+}
